2ae55d04da5d4bb52af399e43901565d43613658
Hanksito <barjabalseiro@gmail.com>
2022-10-13 12:00:09 +0200

    empezando con el formulario de pacientes

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,5 +1,14 @@
 const Formulario = () => {
-  return <h2>Formulario</h2>;
+  return (
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <p className="texty-lg mt-5 text-center">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+      <form></form>
+    </div>
+  );
 };
 
 export default Formulario;
